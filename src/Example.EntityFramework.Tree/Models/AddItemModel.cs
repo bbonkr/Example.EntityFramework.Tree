a1885135b5b60e66ee6ba5bc679cb1a6ea6c6047
Pon Cheol Ku (구본철) <bbon@live.com>
2022-09-27 16:25:49 +0900

    Add validators #14

--- src/Example.EntityFramework.Tree/Models/AddItemModel.cs
+++ src/Example.EntityFramework.Tree/Models/AddItemModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json.Serialization;
+using FluentValidation;
 
 namespace Example.EntityFramework.Tree.Models;
 
@@ -13,4 +14,20 @@ public class AddItemModel
     public int Order { get; set; } = 0;
 
     public Guid? ParentId { get; set; } = null;
+}
+
+public class AddItemModelValidator : AbstractValidator<AddItemModel>
+{
+    public AddItemModelValidator()
+    {
+        RuleFor(x => x.LanguageCode).NotEmpty()
+            .WithErrorCode("languagecode_required")
+            .WithMessage(x => $"{nameof(AddItemModel.LanguageCode)} is required");
+        RuleFor(x => x.Name).NotEmpty()
+            .WithErrorCode("name_required")
+            .WithMessage(x => $"${nameof(AddItemModel.Name)} is required");
+        RuleFor(x => x.Order).GreaterThan(0)
+            .WithErrorCode("order_invalid")
+            .WithMessage(x => $"{nameof(AddItemModel.Order)} is greater than 0");
+    }
 }
--- src/Example.EntityFramework.Tree/Models/UpdateItemModel.cs
+++ src/Example.EntityFramework.Tree/Models/UpdateItemModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json.Serialization;
+using FluentValidation;
 
 namespace Example.EntityFramework.Tree.Models;
 
@@ -16,4 +17,20 @@ public class UpdateItemModel
     public int Order { get; set; } = 0;
 
     public Guid? ParentId { get; set; } = null;
+}
+
+public class UpdateItemModelValidator : AbstractValidator<UpdateItemModel>
+{
+    public UpdateItemModelValidator()
+    {
+        RuleFor(x => x.LanguageCode).NotEmpty()
+            .WithErrorCode("languagecode_required")
+            .WithMessage(x => $"{nameof(UpdateItemModel.LanguageCode)} is required");
+        RuleFor(x => x.Name).NotEmpty()
+            .WithErrorCode("name_required")
+            .WithMessage(x => $"{nameof(UpdateItemModel.Name)} is required");
+        RuleFor(x => x.Order).GreaterThan(0)
+            .WithErrorCode("order_invalid")
+            .WithMessage(x => $"{nameof(UpdateItemModel.Order)} is greater than 0");
+    }
 }
