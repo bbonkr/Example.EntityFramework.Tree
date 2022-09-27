0a354d7fcfcf7659bd6e20b61f3123fb756efa68
Pon Cheol Ku (구본철) <bbon@live.com>
2022-09-27 16:25:35 +0900

    Configure #14

---
+++ src/Example.EntityFramework.Tree/Extensions/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+using FluentValidation;
+
+namespace Example.EntityFramework.Tree.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddValidators(this IServiceCollection services, Type? type = null, ServiceLifetime serviceLifetime = ServiceLifetime.Scoped)
+    {
+        Assembly assembly;
+        if (type == null)
+        {
+            assembly = Assembly.GetExecutingAssembly();
+        }
+        else
+        {
+            assembly = type.Assembly;
+        }
+
+        services.AddValidatorsFromAssemblies(new List<Assembly> { assembly }, serviceLifetime);
+
+        return services;
+    }
+}
+
--- src/Example.EntityFramework.Tree/Program.cs
+++ src/Example.EntityFramework.Tree/Program.cs
@@ -2,9 +2,14 @@ using System.Text.Json;
 using Example.EntityFramework.Tree.Data;
 using Example.EntityFramework.Tree.Data.SqlServer;
 using Example.EntityFramework.Tree.Options;
+using FluentValidation.AspNetCore;
 using kr.bbon.AspNetCore.Extensions.DependencyInjection;
 using kr.bbon.AspNetCore.Filters;
 using Microsoft.EntityFrameworkCore;
+using Example.EntityFramework.Tree.Extensions.DependencyInjection;
+using kr.bbon.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using Example.EntityFramework.Tree.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -43,7 +48,36 @@ builder.Services.AddControllers(options =>
     {
         options.JsonSerializerOptions.AllowTrailingCommas = true;
         options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+    })
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        options.InvalidModelStateResponseFactory = context =>
+        {
+            PathString path = context.HttpContext.Request.Path;
+            string method = context.HttpContext.Request.Method;
+            string displayName = context.ActionDescriptor.DisplayName ?? string.Empty;
+
+            var errors = context.ModelState.Values
+                .SelectMany(x => x.Errors)
+                .Select(error => JsonSerializer.Deserialize<ErrorModel>(error.ErrorMessage));
+
+            var responseStatusCode = StatusCodes.Status400BadRequest;
+            var responseModel = kr.bbon.AspNetCore.Models.ApiResponseModelFactory.Create(responseStatusCode, "Payload is invalid", errors);
+
+            responseModel.Path = path.ToString();
+            responseModel.Method = method;
+            responseModel.Instance = displayName;
+
+            context.HttpContext.Response.StatusCode = responseStatusCode;
+
+            return new ObjectResult(responseModel);
+        };
     });
+
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddValidators();
+
+builder.Services.AddTransient<IValidatorInterceptor, CustomValidatorInterceptor>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
